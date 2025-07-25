5b4e00f593b5593a4ac3ad2cee6577e5e6dcd22a
Kusk24 <winyumg2003a@gmail.com>
2025-07-25 08:41:21 +0700

    feat: NotFoundPage added

--- src/main.jsx
+++ src/main.jsx
@@ -11,8 +11,8 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import AnalyticsDashboard from './Dashboard.jsx';
+import { createBrowserRouter } from 'react-router-dom';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
